d4d370284588fc3c4e0dbebe334b32fe27d8fdfc
Antonio Marucci <antoniomarucci13@gmail.com>
2024-01-22 17:44:18 +0100

    finite le bestemmie del primo giorno, ciao

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Profile from "./components/Profile";
 import Rete from "./components/Rete";
+import MyNav from "./components/MyNav";
 
 function App() {
   return (
     <div>
+      <MyNav />
       <Rete />
       <Profile />
     </div>
