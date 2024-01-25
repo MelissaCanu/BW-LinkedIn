0d71a1519b1420b86b51e9ee329e5de79b1e2a07
MelissaCanu <melissamcanu@gmail.com>
2024-01-25 12:59:43 +0100

    new component, fetching CategoryOrCompany

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,38 @@ import Home from "./components/homeComponents/Home";
 import NewsSide from "./components/homeComponents/NewsSide";
 import { Container, Row, Col } from "react-bootstrap";
 import Lavoro from "./components/lavoroComponents/Lavoro";
+import JobSpecifics from "./components/lavoroComponents/JobSpecifics";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <MyNav />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Container>
-                <Row>
-                  <Col className="col-md-12 col-lg-8">
-                    <Home />
-                  </Col>
-                  <Col className="d-none d-lg-block col-lg-4">
-                    <NewsSide />
-                  </Col>
-                </Row>
-              </Container>
-            }
-          />
-          <Route path="/network" element={<Rete />} />
-          <Route path="/work" element={<Lavoro />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div>
+				<MyNav />
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<Container>
+								<Row>
+									<Col className="col-md-12 col-lg-8">
+										<Home />
+									</Col>
+									<Col className="d-none d-lg-block col-lg-4">
+										<NewsSide />
+									</Col>
+								</Row>
+							</Container>
+						}
+					/>
+					<Route path="/network" element={<Rete />} />
+					<Route path="/work" element={<Lavoro />} />
+					<Route path="/profile" element={<Profile />} />
+					<Route path="/job/:categoryOrCompany" element={<JobSpecifics />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
